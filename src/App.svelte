b0557a996751c502a0e88dfdf25f5dd9a2735ed4
pizzalawl <pizzalawls@gmail.com>
2024-06-30 23:24:55 -0500

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Game from './lib/Game.svelte'
+</script>
+
+<main>
+  <!-- Main flexbox organizer -->
+  <div class="d-flex flex-row p-2 m-2">
+
+    <!-- Main Game Component and Chatbox -->
+    <div class="d-flex flex-column">
+      <Game/>
+      <div class="border border-info p-2 m-2">Chat</div>
+    </div>
+
+    <!-- Information/Control Section -->
+    <div class="border border-info p-2 m-2 ">
+      <h3 class="text-center bold"> Frisk!</h3>
+      <h6 class="text-center">The classic game about world domination!</h6>
+      <button type="button" class="btn btn-success">Play</button>
+    </div>
+  
+  </div>
+</main>
---
+++ src/lib/Game.svelte
@@ -0,0 +1,21 @@
+<script>
+// @ts-nocheck
+
+     import { onMount } from 'svelte';
+
+    function init(){
+        let stage = new createjs.Stage('game');
+
+        let circle = new createjs.Shape();
+        circle.graphics.beginFill("red").drawCircle(0, 0, 40);
+        circle.x = circle.y = 50;
+        stage.addChild(circle);
+        stage.update();
+    }
+
+    onMount(() => {
+        init();
+    });
+</script>
+
+<canvas class="border border-primary p-2 m-2" width="700" height="500" id="game"></canvas>
